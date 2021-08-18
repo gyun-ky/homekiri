716ca44491a0a18ac3a6352f40890fe21834107d
kim_ki_yun <kiyoon9390@gmail.com>
2021-08-18 19:26:39 +0900

    [FIX] commit

--- src/main/java/com/example/homekiri/user/UserRepository.java
+++ src/main/java/com/example/homekiri/user/UserRepository.java
@@ -1,7 +1,25 @@
 package com.example.homekiri.user;
 
+import com.example.homekiri.user.model.User;
 import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.lang.reflect.Member;
 
 @Repository
 public class UserRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public void save(User user){
+        em.persist(user);
+    }
+
+    public Member findUser(Long userIdx){
+        return em.find(Member.class, userIdx);
+    }
+
+
 }
