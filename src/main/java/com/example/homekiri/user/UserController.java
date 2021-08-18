f5cb558dbe3b969c7c2fdd82ebde949544c1d189
kim_ki_yun <kiyoon9390@gmail.com>
2021-08-18 17:24:39 +0900

    [FIX] jpa application.yml 수정

--- src/main/java/com/example/homekiri/user/UserController.java
+++ src/main/java/com/example/homekiri/user/UserController.java
@@ -5,10 +5,7 @@ import com.example.homekiri.config.BaseResponse;
 import com.example.homekiri.config.BaseResponseStatus;
 import com.example.homekiri.user.model.GetMypageRes;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/web/users")
