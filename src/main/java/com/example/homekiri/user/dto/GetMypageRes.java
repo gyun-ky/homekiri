e9a07dab330960ddb9c10f83f1f2ef30da17a16b
kim_ki_yun <kiyoon9390@gmail.com>
2021-09-03 15:38:26 +0900

    [FEAT] mypage jwt

--- src/main/java/com/example/homekiri/user/dto/GetMypageRes.java
+++ src/main/java/com/example/homekiri/user/dto/GetMypageRes.java
@@ -2,9 +2,11 @@ package com.example.homekiri.user.dto;
 
 import com.example.homekiri.user.model.User;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.Setter;
 
 @Setter
+@Getter
 @AllArgsConstructor
 public class GetMypageRes {
     private String nickname;
