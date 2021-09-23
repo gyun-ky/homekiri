06b69383a9b151c4b264fc1535754e4cf0f01781
kim_ki_yun <kiyoon9390@gmail.com>
2021-09-23 17:30:00 +0900

    [FEAT] signIn user create error complete

---
+++ src/main/java/com/example/homekiri/config/Auditable.java
@@ -0,0 +1,23 @@
+package com.example.homekiri.config;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import java.time.LocalDateTime;
+
+@Getter(AccessLevel.PROTECTED)
+@Setter(AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+public class Auditable {
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
