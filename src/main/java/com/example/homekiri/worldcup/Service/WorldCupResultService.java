0fc520849ff9c0edddb2f155f8b22ffdabf97183
minlee <lucky3749@naver.com>
2021-10-12 17:42:18 +0900

    [feature/worldcup] final-2

--- src/main/java/com/example/homekiri/worldcup/Service/WorldCupResultService.java
+++ src/main/java/com/example/homekiri/worldcup/Service/WorldCupResultService.java
@@ -121,17 +121,32 @@ public class WorldCupResultService {
                 if (name.equals("netflix")){
                     mediaSurvey.setNetflix(mediaSurvey.getNetflix() + data.getPoint());
                 }
-                if (name.equals("roman")){
-                    mediaSurvey.setNetflix(mediaSurvey.getNetflix() + data.getPoint());
+                if (name.equals("romance")){
+                    mediaSurvey.setRomance(mediaSurvey.getRomance() + data.getPoint());
                 }
-                if (name.equals("netflix")){
-                    mediaSurvey.setNetflix(mediaSurvey.getNetflix() + data.getPoint());
+                if (name.equals("sbs")){
+                    mediaSurvey.setSbs(mediaSurvey.getSbs() + data.getPoint());
                 }
-                if (name.equals("netflix")){
-                    mediaSurvey.setNetflix(mediaSurvey.getNetflix() + data.getPoint());
+                if (name.equals("scienceFiction")){
+                    mediaSurvey.setScienceFiction(mediaSurvey.getScienceFiction() + data.getPoint());
                 }
-                if (name.equals("netflix")){
-                    mediaSurvey.setNetflix(mediaSurvey.getNetflix() + data.getPoint());
+                if (name.equals("twShow")){
+                    mediaSurvey.setTvShow(mediaSurvey.getTvShow() + data.getPoint());
+                }
+                if (name.equals("twShow")){
+                    mediaSurvey.setTvShow(mediaSurvey.getTvShow() + data.getPoint());
+                }
+                if (name.equals("tving")){
+                    mediaSurvey.setTving(mediaSurvey.getTving() + data.getPoint());
+                }
+                if (name.equals("tvn")){
+                    mediaSurvey.setTvn(mediaSurvey.getTvn() + data.getPoint());
+                }
+                if (name.equals("watch")){
+                    mediaSurvey.setWatcha(mediaSurvey.getWatcha() + data.getPoint());
+                }
+                if (name.equals("wave")){
+                    mediaSurvey.setWave(mediaSurvey.getWave() + data.getPoint());
                 }
             }
         }
